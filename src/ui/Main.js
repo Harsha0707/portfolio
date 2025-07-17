4326899588aaf41933b1e897ac5e23e68b77f5f3
Harsha0707 <harsha020296@gmail.com>
2025-07-17 18:50:19 +0530

    Updated Main.js

--- src/ui/Main.js
+++ src/ui/Main.js
@@ -1,7 +1,6 @@
 import About from "./Main/About";
 import ContactMe from "./Main/ContactMe";
 import Experience from "./Main/Experience";
-import WorkProjects from "./Main/WorkProjects";
 import Projects from "./Main/Projects";
 import Skills from "./Main/Skills";
 import Education from "./Main/Education";
